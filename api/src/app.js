16ff0f8b14282a2ddfdc267df30f1b170c32607d
Evgenii <7zhhovalyg@mail.ru>
2026-04-20 14:33:56 +0300

    Remove cors

--- api/src/app.js
+++ api/src/app.js
@@ -1,44 +1,10 @@
 import "dotenv/config"
-import cors from "cors"
 import express from "express"
 import router from "./routes/index.js"
 import cookieParser from "cookie-parser"
 
 const PORT = process.env.PORT || 8000
-const allowedOrigins = new Set(
-  process.env.ALLOWED_ORIGINS.split(",").map((o) => o.trim()),
-)
 const app = express()
-
-app.use(
-  cors({
-    origin(origin, callback) {
-      if (!origin) return callback(null, true)
-
-      if (allowedOrigins.has(origin)) {
-        return callback(null, origin)
-      }
-
-      return callback(new Error("Not allowed by CORS"))
-    },
-    credentials: true,
-  }),
-)
-const allowedOriginsArray = [...allowedOrigins]
-
-app.use((req, res, next) => {
-  const origin = req.headers.origin
-
-  if (["GET", "HEAD", "OPTIONS"].includes(req.method)) {
-    return next()
-  }
-
-  if (!origin || !allowedOriginsArray.includes(origin)) {
-    return res.status(403).json({ message: "CSRF blocked" })
-  }
-
-  next()
-})
 app.use(express.json({ limit: "10mb" }))
 app.use(express.urlencoded({ limit: "10mb", extended: true }))
 app.use(cookieParser())
